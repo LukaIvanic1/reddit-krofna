dbb275f5b9ec36aec9ab29b51fad2257720b782b
Luka Ivanić <107309593+LukaIvanic1@users.noreply.github.com>
2023-01-24 10:17:52 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        
+        <h1>PENIS LOOKUP!!!</h1>
+        <div className='rowC'>
+          <form>
+
+
+            <label className='marginilized'>
+              u/
+
+            </label>
+            <input type="text" name="name" className='text-black rounded-lg mjau' />
+          </form>
+
+          <form >
+            <label className='marginilized'>
+              u/
+
+            </label>
+            <input type="text" name="name" className='text-black rounded-lg mjau' />
+            
+
+          </form>
+        </div>
+        <button class="rounded-full mjau">SEARCH</button>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
